0cfd5a2a2378a2bdb0321c56c77bb19d3f363fae
rusira <rusirajayatilake96@gmail.com>
2024-02-20 14:36:33 +1300

    flutter search screen

--- clients/mobile/lib/components/listview_card.dart
+++ clients/mobile/lib/components/listview_card.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/material.dart';
 
-class ListViewCard extends StatefulWidget {
-  const ListViewCard({super.key});
+class CustomCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String imgUrl;
 
-  @override
-  State<ListViewCard> createState() => _ListViewCardState();
-}
+  CustomCard({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.imgUrl,
+  }) : super(key: key);
 
-class _ListViewCardState extends State<ListViewCard> {
-  //  // var img =
-//     //     'https://plus.unsplash.com/premium_photo-1675745329954-9639d3b74bbf?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 100,
-      color: Colors.blueAccent,
-      child: const Column(
-        children: [
-          Text("Text"),
-        ],
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      // color: Colors.blueAccent,
+      child: Container(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.network(
+                imgUrl,
+                width: 100, // Constrain the width of the image
+                height: 100, // Constrain the height of the image
+                fit: BoxFit.cover, // Adjust the image's aspect ratio
+              ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.black, // Set the text color
+                    ),
+                  ),
+                  Text(
+                    subtitle,
+                    style: const TextStyle(
+                      color: Colors.black, // Set the text color
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- clients/mobile/lib/screens/search_screen.dart
+++ clients/mobile/lib/screens/search_screen.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/components/listview_card.dart';
-
-/* Things to Fix
-  * The Search UI is not showing,
-  when the list view is given.
-  * The ListView is not properly setup.
-*/
+import 'package:mobile/components/navigation_bar.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -15,52 +10,58 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  var img =
+      'https://plus.unsplash.com/premium_photo-1675745329954-9639d3b74bbf?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Column(
-            children: [
-              const SizedBox(
-                height: 45,
-              ),
-              // Search bar
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Row(
-                  children: [
-                    Icon(Icons.search),
-                    SizedBox(width: 10),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Search...',
-                          border: InputBorder.none,
-                        ),
-                      ),
+          const SizedBox(
+            height: 45,
+          ),
+          // Search bar
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Row(
+              children: [
+                Icon(Icons.search),
+                SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search...',
+                      border: InputBorder.none,
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 10, // Adjust the height as needed
-              ),
-              // Listview
-              ListView.builder(
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  return const ListViewCard();
-                },
-              ),
-            ],
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 8, // Adjust the height as needed
+          ),
+          // Listview
+          Expanded(
+            child: ListView.builder(
+              // 2. Add ListView.builder
+              itemCount: 20, // Number of items in your list
+              itemBuilder: (context, index) {
+                return CustomCard(
+                  title: 'Item $index',
+                  subtitle: 'Description of item $index',
+                  imgUrl: img,
+                );
+              },
+            ),
           ),
         ],
       ),
+      bottomNavigationBar: const AppNavigationBar(),
     );
   }
 }
